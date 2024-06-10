348c12107abb434994ad2dfecb2d938227b39ec6
Heonbyeong <abh0920one@gmail.com>
2024-06-10 17:40:46 +0900

    create: project generate

---
+++ src/main/kotlin/com/example/kopring_practice/KopringPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopring_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KopringPracticeApplication>(*args)
+}
